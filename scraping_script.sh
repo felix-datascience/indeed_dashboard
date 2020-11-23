15f998e41bcdec46e17751b86138ef6dbd22fe63
Felix <felix.vogl@gmx.de>
2020-11-23 16:15:28 +0100

    Add bash script that runs scrapy scraper on Raspberry Pi

---
+++ scraping_script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Script for running scraper, putting scraped data file in project directory
+# and pushing changes to GitHub
+
+YearStart=`date +%Y`
+MonthStart=`date +%m`
+DayStart=`date +%d`
+HourStart=`date +%H`
+MinuteStart=`date +%M`
+SecondStart=`date +%S`
+DateStart=$"$YearStart-$MonthStart-$DayStart"
+TimeStampStart=$"[$DateStart $HourStart:$MinuteStart:$SecondStart]"
+
+ProjectRootFolder=$"/home/pi/indeed_dashboard_deployed"
+ScraperFolder=$"/home/pi/indeed_dashboard/scraper"
+DataFolder=$"$ProjectRootFolder/data"
+FileName=$"$DataFolder/$YearStart-$MonthStart-$DayStart.json"
+
+# Print message when starting
+echo "$TimeStampStart Start scraping script..."
+
+# Check if file already exists
+
+
+# Change directory to where scraper is located
+cd $ScraperFolder
+
+# Scraping
+PATH=$PATH:/usr/local/bin
+export PATH
+scrapy crawl jobs -o $FileName
+#sleep 5
+#touch $FileName
+
+# Change directory to root directory of project
+cd $ProjectRootFolder
+
+# Add new file to stage area, commit and push to GitHub repository
+CommitMessage=$"Automatic commit: Add scraped date from $DateStart"
+#CommitMessage=$"Data pipeline test run $TimeStampStart"
+git add .
+git commit -m "$CommitMessage"
+git push
+
+# Print message when done
+YearEnd=`date +%Y`
+MonthEnd=`date +%m`
+DayEnd=`date +%d`
+HourEnd=`date +%H`
+MinuteEnd=`date +%M`
+SecondEnd=`date +%S`
+DateEnd=$"$YearEnd-$MonthEnd-$DayEnd"
+TimeStampEnd=$"[$DateEnd $HourEnd:$MinuteEnd:$SecondEnd]"
+echo "$TimeStampEnd Done"
